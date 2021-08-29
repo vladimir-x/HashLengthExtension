16c64c8f47c3422decd39b5381c54e7b77652af6
dude <vladimir.x89@mail.ru>
2021-08-29 12:38:20 +0300

    first commit

---
+++ src/main/java/ru/dude/hash_length_extension/Runner.java
@@ -0,0 +1,12 @@
+package ru.dude.hash_length_extension;
+
+/**
+ * @author Vladimir Hrostitisky
+ * Date: 29.08.2021
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+        System.out.println("111");
+    }
+}
